89f4962fca6e9cb434afff66ae9ab7c1efc3370f
Munki Albright <stanlyjohnson10@gmail.com>
2020-09-10 17:14:32 +0100

    Search engine, user login and registration added

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,11 @@ from blog import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register', views.register, name='register'),
+    path('login_view', views.login_view, name='login_view'),
     path('blog', views.blog, name='blog'),
     path('blogpost/<str:slug>', views.blogpost, name='blogpost'),
     path('editor', views.editor, name='editor'),
     path('contact', views.contact, name='contact'),
     path('search', views.search, name='search')
-]
+] 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from blog.models import Blog, Contact
 import math
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def register(request):
+    context = {'message': ''}
+    if request.method == 'POST':
+        user_e = User.objects.all().filter(Q(username= request.POST.get("username",False)) | Q(email= request.POST.get("email",False)))
+        if user_e:
+            context = {'message': 'Username or email is already used! Please kindly change'}
+            return render(request, 'register.html', context)
+        else:
+            if request.POST.get("pswd",False) == request.POST.get("c_pswd",False):
+                user=User.objects.create_user(username= request.POST.get("username",False),email= request.POST.get("email",False),password=request.POST.get("pswd",False))
+                user.first_name=request.POST.get("fistName",False)
+                user.last_name=request.POST.get("lastName",False)
+                user.save()
+                if user is not None:
+                    user_authenticate = authenticate(request, username= request.POST.get("username",False), password=request.POST.get("password",False))
+                    login(request,user_authenticate)  
+                    return redirect('home')
+                else:
+                    context = {'message': 'Please fill all fields'}
+                    return render(request, 'register.html', context)
+            else:
+                context = {'message': 'Passwords don`t match!'}
+                return render(request, 'register.html', context)
+
+        
+    return render(request, 'register.html')
+
+def login_view(request):
+    context = {'message': ''}
+    if request.method == 'POST':
+        user_id = request.POST.get("user_id",False)
+        password = request.POST.get("pswd",False) 
+        user = authenticate(request, username=user_id, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            context = {'message': 'Invalid Credentials'}
+            return render(request, 'login.html', context)
+
+    return render(request, 'login.html')
 
 def blog(request):
     numPosts = 5
@@ -77,4 +122,11 @@ def contact(request):
     return render(request, 'contact.html', context)
 
 def search(request):
-    return render(request, 'search.html')
+    if request.method == 'GET':
+        srch = request.GET.get('search')
+        blogs = Blog.objects.all().filter(Q(title=srch) |
+        Q(author=srch))
+
+        context = {'blogs': blogs, 'srch_input': srch}
+
+    return render(request, 'search.html', context)
